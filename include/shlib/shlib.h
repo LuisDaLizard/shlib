867daa4ec807065023b2b908f7de0485c57dfc58
Luis T. Sanchez <luis.tadeojr@gmail.com>
2023-07-01 23:38:31 -0600

    begin and end drawing sections

--- include/shlib/shlib.h
+++ include/shlib/shlib.h
@@ -18,5 +18,7 @@ extern void window_toggle_fullscreen(void);
 
 // ------------ Shlib Core Graphics API ------------ //
 extern void graphics_clear_screen(float color[4]);
+extern void graphics_begin_drawing(void);
+extern void graphics_end_drawing(void);
 
 #endif //SHLIB_SHLIB_H
--- src/shlib_core.c
+++ src/shlib_core.c
@@ -71,3 +71,14 @@ void graphics_clear_screen(float color[4])
     glClearColor(color[0], color[1], color[2], color[3]);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
+
+void graphics_begin_drawing(void)
+{
+    window_poll_events();
+}
+
+void graphics_end_drawing(void)
+{
+
+    window_swap_buffers();
+}
